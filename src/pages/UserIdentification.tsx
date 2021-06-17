5cfd4b4cab344cf5d094b868a4209e2b884a641f
Henrique Brites <henriquebrites@live.com>
2021-06-17 02:47:31 -0300

    applying a UX concept in UserIdentification and modifying the Button;add config for code style; create header and list for PlantSelect screen; finish PlantSelect screen; insert name in Header rocket; refactor PlantSelect; create PlantSave screen rocket; refactor Confirmation screen; create MyPlants and finish PlantSave screen; create tabRoutes; creating remove plant function; configuring app notifications

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {
+  Alert,
   Keyboard,
   KeyboardAvoidingView,
   Platform,
@@ -11,6 +12,7 @@ import {
   View
 } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { Button } from '../components/Button';
 
@@ -39,8 +41,23 @@ export function UserIdentification() {
 
   }
   
-  function handleSubmit(){
-    navigation.navigate('Confirmation');
+  async function handleSubmit(){
+    if(!name) 
+      return Alert.alert('Me diz como chamar você 😢');
+
+    try {
+      await AsyncStorage.setItem('@plantmanager:user', name);
+      navigation.navigate('Confirmation', {
+        title: 'Prontinho',
+        subtitle: 'Agora vamos começar a cuidar das suas plantinhas com muito cuidado.',
+        buttonTitle: 'Começar',
+        icon: 'smile',
+        nextScreen: 'PlantSelect',
+      });  
+    } catch {
+      Alert.alert('Não foi possível salvar o seu nome. 😢');
+    }
+    
   }
 
   return (
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -4,8 +4,10 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
+import { PlantSave } from '../pages/PlantSave';
 
 import colors from '../styles/colors';
+import AuthRoutes from './tab.routes';
 
 const stackRoutes = createStackNavigator();
 
@@ -31,6 +33,21 @@ const AppRoutes: React.FC = () => (
     <stackRoutes.Screen 
       name="Confirmation"
       component={Confirmation}
+    />
+
+    <stackRoutes.Screen 
+      name="PlantSelect"
+      component={AuthRoutes}
+    />
+
+    <stackRoutes.Screen 
+      name="PlantSave"
+      component={PlantSave}
+    />
+
+    <stackRoutes.Screen 
+      name="MyPlants"
+      component={AuthRoutes}
     />
 
   </stackRoutes.Navigator>
